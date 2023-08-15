c507ce9b3bd27c707fd90cd3a906ba65b12722af
dheerajjha3123 <dheerajjha11111@gmail.com>
2023-08-15 18:43:31 +0530

    SingUp Page

---
+++ src/main/java/com/Test/Registration/Registration.java
@@ -0,0 +1,64 @@
+package com.Test.Registration;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/register")
+public class Registration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname= request.getParameter("name");
+		String upass= request.getParameter("pass");
+		String uemail= request.getParameter("email");
+		String umobile= request.getParameter("contact");
+		RequestDispatcher dispatcher = null;
+		Connection con=null;
+		
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/singup?useSSL=false","root","Test@123");
+		PreparedStatement pst = con.prepareStatement("insert into user(uname,upass,uemail,umobile) values(?,?,?,?)");
+	pst.setString(1, uname);
+	pst.setString(2, upass);
+	pst.setString(3, uemail);
+	pst.setString(4, umobile);
+	
+	int RowCount = pst.executeUpdate();
+	dispatcher = request.getRequestDispatcher("registration.jsp");
+	if(RowCount>0) {
+		request.setAttribute("status", "success");
+	}
+	else {
+		request.setAttribute("status", "failed");
+
+	}
+	dispatcher.forward(request, response);
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}finally {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	}
+
+}
